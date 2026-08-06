40e7143e552b1f6fa3d950cd9e5586cc43a722b8
mdswaley <mdswaley075@gmail.com>
2026-08-06 13:16:53 +0530

    Define ThreadPoolExecutor class with thread rejection

--- src/main/java/com/example/LearnAsyncScheduling/LearnAsyncSchedulingApplication.java
+++ src/main/java/com/example/LearnAsyncScheduling/LearnAsyncSchedulingApplication.java
@@ -18,6 +18,7 @@ public class LearnAsyncSchedulingApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception{
+
         ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(4,
                 6, 2, TimeUnit.SECONDS, new ArrayBlockingQueue<>(10),
 //                will not show thread rejected exception
@@ -33,33 +34,6 @@ public class LearnAsyncSchedulingApplication implements CommandLineRunner {
 
         );
 
-//        Scheduled ThreadPool Executor
-        ScheduledThreadPoolExecutor scheduledThreadPoolExecutor = new ScheduledThreadPoolExecutor(6, new ThreadFactory() {
-            @Override
-            public Thread newThread(Runnable r) {
-                log.info("");
-                return new Thread(r, "thread "+System.nanoTime());
-            }
-        });
-
-        scheduledThreadPoolExecutor
-                .schedule(new LongRunningTask("schedule task"),
-                        4, TimeUnit.SECONDS);
-
-
-        log.info("Starting main Thread -> {}", Thread.currentThread().getName());
-//        Task
-//         Future<String> res = threadPoolExecutor.submit(()-> {
-//        if you want to return something from submit method using Future you can.
-
-//      using for loop start task is print 16. why :-
-//      bcz we have active task is 6 inside the thread pool and other 10 task are in ArrayQueue. So, once active task is going to complete the array task is going to work
-        for (int i = 0; i < 20; i++) {
-            threadPoolExecutor.submit(new LongRunningTask(i+" "));
-            Thread.sleep(1000);
-        }
-
-        log.info("Starting main Thread -> {}", Thread.currentThread().getName());
 
     }
 
