f342073fffeaedbf4f91a5b96e69d9a76cb17d2d
mdswaley <mdswaley075@gmail.com>
2026-08-12 11:25:03 +0530

    Using ConditionalOnProperty you can define methods are not execute

--- src/main/java/com/example/LearnAsyncScheduling/mySchedule.java
+++ src/main/java/com/example/LearnAsyncScheduling/mySchedule.java
@@ -1,11 +1,16 @@
 package com.example.LearnAsyncScheduling;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
 @Slf4j
+@ConditionalOnProperty(
+        name = {"scheduler.myTask1.enabled", "scheduler.myTask2.enabled"},
+        havingValue = "true"
+)
 public class mySchedule {
 
     @Scheduled(fixedRate = 1000) // Not concurrent (only use one thread)
@@ -28,6 +33,16 @@ public class mySchedule {
             throw new RuntimeException(e);
         }
         log.info("Scheduler2 Ended..{}", Thread.currentThread().getName());
+    }
+
+    @Scheduled(fixedDelay = 2000)
+    public void myTask3() {
+        log.info("Scheduler started..{}", Thread.currentThread().getName());
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 
