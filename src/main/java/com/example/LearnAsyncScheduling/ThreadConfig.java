8ff74dd21682969c0c43619d42e73f8962acb143
mdswaley <mdswaley075@gmail.com>
2026-08-13 12:15:33 +0530

    configure TaskScheduler to make task work parallel

---
+++ src/main/java/com/example/LearnAsyncScheduling/ThreadConfig.java
@@ -0,0 +1,19 @@
+package com.example.LearnAsyncScheduling;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+
+@Configuration
+public class ThreadConfig {
+    @Bean
+    public TaskScheduler taskScheduler(){
+        ThreadPoolTaskScheduler taskScheduler = new ThreadPoolTaskScheduler();
+        taskScheduler.setPoolSize(4);
+        taskScheduler.setThreadNamePrefix("CustomTask-");
+        taskScheduler.initialize();
+
+        return taskScheduler;
+    }
+}
--- src/main/java/com/example/LearnAsyncScheduling/mySchedule.java
+++ src/main/java/com/example/LearnAsyncScheduling/mySchedule.java
@@ -9,44 +9,44 @@ import org.springframework.stereotype.Component;
 @Slf4j
 public class mySchedule {
 
-//    @Scheduled(fixedRate = 1000) // Not concurrent (only use one thread)
-//    public void myTask1() {
-//        log.info("Scheduler1 started..{}", Thread.currentThread().getName());
-//        try {
-//            Thread.sleep(2000);
-//        } catch (InterruptedException e) {
-//            throw new RuntimeException(e);
-//        }
-//        log.info("Scheduler1 Ended..{}", Thread.currentThread().getName());
-//    }
-//
-//    @Scheduled(fixedRate = 1000)
-//    public void myTask2() {
-//        log.info("Scheduler2 started..{}", Thread.currentThread().getName());
-//        try {
-//            Thread.sleep(2000);
-//        } catch (InterruptedException e) {
-//            throw new RuntimeException(e);
-//        }
-//        log.info("Scheduler2 Ended..{}", Thread.currentThread().getName());
-//    }
-
-    @Scheduled(fixedDelay = 2000, initialDelay = 10000) // wait for myTask3 to complete first then wait for 2s
-//    initialDelay means after 10s that method is going to start for first time then every 2s + 1s inside method will execute
-    public void myTask3() {
-        // Task starts
-        log.info("Scheduler started..{}", Thread.currentThread().getName());
-
+    @Scheduled(fixedRate = 1000) // Not concurrent (only use one thread)
+    public void myTask1() {
+        log.info("Scheduler1 started..{}", Thread.currentThread().getName());
         try {
-            // Task takes 1 second to complete
-            Thread.sleep(1000);
+            Thread.sleep(2000);
         } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
+            throw new RuntimeException(e);
         }
-
-        // After the method finishes, Spring waits 2 seconds
-        // before starting myTask3() again.
+        log.info("Scheduler1 Ended..{}", Thread.currentThread().getName());
     }
+//
+    @Scheduled(fixedRate = 1000)
+    public void myTask2() {
+        log.info("Scheduler2 started..{}", Thread.currentThread().getName());
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        log.info("Scheduler2 Ended..{}", Thread.currentThread().getName());
+    }
+
+//    @Scheduled(fixedDelay = 2000, initialDelay = 10000) // wait for myTask3 to complete first then wait for 2s
+////    initialDelay means after 10s that method is going to start for first time then every 2s + 1s inside method will execute
+//    public void myTask3() {
+//        // Task starts
+//        log.info("Scheduler started..{}", Thread.currentThread().getName());
+//
+//        try {
+//            // Task takes 1 second to complete
+//            Thread.sleep(1000);
+//        } catch (InterruptedException e) {
+//            Thread.currentThread().interrupt();
+//        }
+//
+//        // After the method finishes, Spring waits 2 seconds
+//        // before starting myTask3() again.
+//    }
 
 
 }
