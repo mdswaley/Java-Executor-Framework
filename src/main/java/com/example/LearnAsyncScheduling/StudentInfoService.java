27f8198a4f50816ff172657c2ba20c383858832d
mdswaley <mdswaley075@gmail.com>
2026-08-16 04:01:04 +0530

    create methods inside StudentInfoService

--- src/main/java/com/example/LearnAsyncScheduling/StudentInfoService.java
+++ src/main/java/com/example/LearnAsyncScheduling/StudentInfoService.java
@@ -1,5 +1,32 @@
 package com.example.LearnAsyncScheduling;
 
-public interface StudentInfoService {
-    public Student getStudentInfo();
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class StudentInfoService {
+    public String getName() throws InterruptedException{
+        log.info("Getting the name..{}", Thread.currentThread().getName());
+        Thread.sleep(2000);
+        log.info("Returning name");
+
+        return "MD";
+    }
+
+    public String getId() throws InterruptedException{
+        log.info("Getting the Id..{}", Thread.currentThread().getName());
+        Thread.sleep(2000);
+        log.info("Returning Id");
+
+        return "947937342";
+    }
+
+    public String getCollege() throws InterruptedException{
+        log.info("Getting the College..{}", Thread.currentThread().getName());
+        Thread.sleep(2000);
+        log.info("Returning College");
+
+        return "CUTM";
+    }
 }
