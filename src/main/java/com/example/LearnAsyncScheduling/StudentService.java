23a0e2912543f527e1a999b13dc0c406ac9005ba
mdswaley <mdswaley075@gmail.com>
2026-08-17 12:39:52 +0530

    Spring Boot Thread Safety

--- src/main/java/com/example/LearnAsyncScheduling/StudentService.java
+++ src/main/java/com/example/LearnAsyncScheduling/StudentService.java
@@ -36,3 +36,20 @@ public class StudentService {
         }
     }
 }
+
+/*
+
+Spring Boot Thread Safety
+
+Spring Beans default to Singleton scope, which means that there will only ever be one
+instance of them while your application is running. This means that all of your
+Controllers, Services, Repositories, etc code will be shared by multiple threads,
+
+which means they will be shared by multiple user requests running at the same time.
+
+• You should be designing all of these components to be stateless.
+• User state should only exist in parameters, and inside functions.
+• It's totally fine for your Repository class to store state about the database url,
+username, and password. These values are not user-specific.
+
+*/
